0e0713a8f7e1d50e379638495434504259450c0e
mrgwbland <mrgwbland@gmail.com>
2023-12-20 15:23:17 +0000

    committed some old improvements, as I can remember, improved piece development and increased ability to play quickly when low on time.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,7 @@ using System.Numerics;
 using System.Linq;
 public class MyBot : IChessBot
 {
+    //int startDepth;
     Dictionary<ulong, float> HashWithScores = new Dictionary<ulong, float>();
     private float Evaluate(Board board)
     {
@@ -15,8 +16,9 @@ public class MyBot : IChessBot
         int endgame = -1;
         PieceList[] pieceLists = board.GetAllPieceLists();
         int piececount = SquareCounter(board.AllPiecesBitboard);
-        //Endgame is true if there are less than 9 pieces left
-        endgame = (piececount < 10) ? 1 : endgame;
+        //Endgame is true if there are less than x pieces left
+        //when true endgame = 1
+        endgame = (piececount < 16) ? 1 : endgame;
         foreach (PieceList list in pieceLists)
         {
             foreach (Piece piece in list)
@@ -32,13 +34,13 @@ public class MyBot : IChessBot
             }
         }
         if (board.IsWhiteToMove) return (whiteScore - blackScore);
-        return blackScore-whiteScore;
+        return blackScore - whiteScore;
     }
     private float PieceEvaluator(Board board, Piece piece, int endgame)
     {
         if (piece.IsPawn)
         {
-            float pawnvalue = 100 + 2 + 2*(endgame == 1 ? (board.IsWhiteToMove ? piece.Square.Rank : 7 - piece.Square.Rank) : 0);
+            float pawnvalue = 100 + 2 * (endgame == 1 ? (board.IsWhiteToMove ? piece.Square.Rank : 7 - piece.Square.Rank) : 0);
             return pawnvalue;
         }
         if (piece.IsKnight)
@@ -51,14 +53,14 @@ public class MyBot : IChessBot
         }
         if (piece.IsRook)
         {
-            return 100 + SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Rook, piece.Square, board));
+            return 500 + 0.1f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Rook, piece.Square, board));
         }
         if (piece.IsQueen)
         {
-            return 900 + SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Queen, piece.Square, board));
+            return 900 + 0.1f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Queen, piece.Square, board));
         }
         //If nothing prior then king
-        return endgame*SquareCounter(BitboardHelper.GetKingAttacks(piece.Square));
+        return endgame * SquareCounter(BitboardHelper.GetKingAttacks(piece.Square));
     }
     private int SquareCounter(ulong bitboard)
     {
@@ -77,6 +79,7 @@ public class MyBot : IChessBot
         {
             float finalEval = quiescenceSearch(board, alpha, beta);
             HashWithScores[board.ZobristKey] = finalEval; // Store the ZobristKey and its associated score
+            //Console.Write("Eval:"+finalEval);
             return finalEval;
         }
         if (board.IsInCheckmate()) return float.NegativeInfinity;
@@ -94,6 +97,12 @@ public class MyBot : IChessBot
         foreach (Move move in legalMoves)
         {
             board.MakeMove(move);
+            //Console.WriteLine();
+            //for(int i=depth;i<=startDepth;i++)
+            //{
+            //    Console.Write("    ");
+            //}
+            //Console.Write(move);
             float eval = -Negamax(board, depth - 1, -beta, -alpha);
             board.UndoMove(move);
             bestEval = Math.Max(bestEval, eval);
@@ -101,7 +110,7 @@ public class MyBot : IChessBot
         }
         return bestEval;
     }
-    private float quiescenceSearch(Board board,float alpha, float beta)
+    private float quiescenceSearch(Board board, float alpha, float beta)
     {
         //Evaluation without captures
         float stand_pat = Evaluate(board);
@@ -129,22 +138,35 @@ public class MyBot : IChessBot
         List<MoveWithScore> movesWithScores = new List<MoveWithScore>();
         Move[] legalMoves = board.GetLegalMoves();
         //Increases search depth if the following parameters are met.
-        if (SquareCounter(board.AllPiecesBitboard) < 10) depth += 2;
+        if (SquareCounter(board.AllPiecesBitboard) < 14) depth += 1;
+        if (SquareCounter(board.AllPiecesBitboard) < 8) depth += 2;
         if (board.IsInCheck()) depth += 1;
-        if (legalMoves.Length == 1) depth += 1;
-        if (timer.MillisecondsRemaining < 1000) depth = 1;
+        if (legalMoves.Length == 1)
+        {
+            return legalMoves[0];
+        }
+        if (timer.MillisecondsRemaining < 5000) depth = 1;
+        if (timer.MillisecondsRemaining < 1000) depth = 0;
+        //startDepth = depth;
         foreach (Move move in legalMoves)
         {
+            //Console.Write("\n"+move);
+            if (timer.MillisecondsRemaining < 500)
+            {
+                movesWithScores = movesWithScores.OrderByDescending(ms => ms.Score).ToList();
+                return movesWithScores[0].Move;
+            }
             board.MakeMove(move);
             if (board.IsInCheckmate())
             {
                 return move;
             }
-            float score = -Negamax(board, depth - 1, float.NegativeInfinity, float.PositiveInfinity);
+            float score = -Negamax(board, depth, float.NegativeInfinity, float.PositiveInfinity);
             board.UndoMove(move);
             movesWithScores.Add(new MoveWithScore { Move = move, Score = score });
         }
         movesWithScores = movesWithScores.OrderByDescending(ms => ms.Score).ToList();
+        //Console.WriteLine(legalMoves.Length + " legal moves, eval " + movesWithScores[0].Score / 100);
         return movesWithScores[0].Move;
     }
     public struct MoveWithScore
