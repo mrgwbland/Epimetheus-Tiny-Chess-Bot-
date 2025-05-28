36e5c8fca2bd56999ca0b16dc21da42b02320628
mrgwbland <mrgwbland@gmail.com>
2025-05-28 15:19:04 +0100

    Added move ordering to searches and thus vastly sped up tree search. Because of this changes to the bots depth management have been made so it will search deeper.

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -11,16 +11,32 @@ namespace ChessChallenge.Example
 
         private float Evaluate(Board board)
         {
-            if (board.IsInCheckmate()) return float.NegativeInfinity;
-            if (board.IsDraw()) return 0;
+            if (board.IsInCheckmate())
+            {
+                return float.NegativeInfinity;
+            }
+
+            if (board.IsDraw())
+            {
+                return 0;
+            }
+
             float whiteScore = 0;
             float blackScore = 0;
-            int endgame = -1;
             PieceList[] pieceLists = board.GetAllPieceLists();
-            int piececount = SquareCounter(board.AllPiecesBitboard);
+            int pieceCount = SquareCounter(board.AllPiecesBitboard);
+
             // Endgame is true if there are less than x pieces left
-            // when true endgame = 1
-            endgame = (piececount < 16) ? 1 : endgame;
+            int endgame;
+            if (pieceCount < 16)
+            {
+                endgame = 1;
+            }
+            else
+            {
+                endgame = -1;
+            }
+
             foreach (PieceList list in pieceLists)
             {
                 foreach (Piece piece in list)
@@ -35,151 +51,222 @@ namespace ChessChallenge.Example
                     }
                 }
             }
-            return board.IsWhiteToMove ? (whiteScore - blackScore) : (blackScore - whiteScore);
+
+            if (board.IsWhiteToMove)
+            {
+                return whiteScore - blackScore;
+            }
+            else
+            {
+                return blackScore - whiteScore;
+            }
         }
 
         private float PieceEvaluator(Board board, Piece piece, int endgame)
         {
             if (piece.IsPawn)
             {
-                float pawnValue = 100 + 2 * (endgame == 1 ? (board.IsWhiteToMove ? piece.Square.Rank : 7 - piece.Square.Rank) : 0);
+                float pawnValue = 100;
+                if (endgame == 1)
+                {
+                    int rank;
+                    if (board.IsWhiteToMove)
+                    {
+                        rank = piece.Square.Rank;
+                    }
+                    else
+                    {
+                        rank = 7 - piece.Square.Rank;
+                    }
+                    pawnValue += 2 * rank;
+                }
+
                 return pawnValue;
             }
+
             if (piece.IsKnight)
             {
                 return 310 + SquareCounter(BitboardHelper.GetKnightAttacks(piece.Square));
             }
+
             if (piece.IsBishop)
             {
                 return 320 + SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Bishop, piece.Square, board));
             }
+
             if (piece.IsRook)
             {
                 return 500 + 0.5f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Rook, piece.Square, board));
             }
+
             if (piece.IsQueen)
             {
                 return 900 + 0.1f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Queen, piece.Square, board));
             }
-            // If nothing prior then king
+
+            // If nothing prior, then the piece must be a king
             return endgame * SquareCounter(BitboardHelper.GetKingAttacks(piece.Square));
         }
 
         private int SquareCounter(ulong bitboard)
         {
-            // Brian Kernighan's algorithm
             int count = 0;
+
             while (bitboard != 0)
             {
                 bitboard &= (bitboard - 1);
                 count++;
             }
+
             return count;
         }
 
-        private float Negamax(Board board, int depth, float alpha, float beta)
+        private (float, Move) Negamax(Board board, int depth, float alpha, float beta)
         {
             if (board.IsInCheckmate())
             {
-                return float.PositiveInfinity;
+                return (float.NegativeInfinity + (depth * 100), Move.NullMove);
             }
+
             if (board.IsDraw())
             {
-                return 0;
+                return (0, Move.NullMove);
             }
+
             if (depth == 0)
             {
-                float finalEval = quiescenceSearch(board, alpha, beta);
-                return finalEval;
+                float finalEval = QuiescenceSearch(board, alpha, beta);
+                return (finalEval, Move.NullMove);
             }
-            if (alpha >= beta)
-            {
-                return alpha;
-            }
-            float bestEval = float.NegativeInfinity;
+
             Move[] legalMoves = board.GetLegalMoves();
+            Move bestMove = Move.NullMove;
+            float bestEval = float.NegativeInfinity;
+
             foreach (Move move in legalMoves)
             {
                 board.MakeMove(move);
-                float eval = -Negamax(board, depth - 1, -beta, -alpha);
+                (float eval, _) = Negamax(board, depth - 1, -beta, -alpha);
+                eval = -eval;
                 board.UndoMove(move);
 
-                bestEval = Math.Max(bestEval, eval);
+                if (eval > bestEval)
+                {
+                    bestEval = eval;
+                    bestMove = move;
+                }
+
                 alpha = Math.Max(alpha, eval);
 
                 if (alpha >= beta)
                 {
-                    break;
+                    break; // Beta cutoff
                 }
             }
-            return bestEval;
+
+            return (bestEval, bestMove);
         }
 
-        private float quiescenceSearch(Board board, float alpha, float beta)
+        private float QuiescenceSearch(Board board, float alpha, float beta)
         {
-            // Evaluation without captures
-            float stand_pat = Evaluate(board);
-            if (stand_pat >= beta)
+            float standPat = Evaluate(board);
+
+            if (standPat >= beta)
             {
                 return beta;
             }
-            if (stand_pat > alpha) alpha = stand_pat;
+
+            if (standPat > alpha)
+            {
+                alpha = standPat;
+            }
+
             Move[] legalMoves = board.GetLegalMoves();
+
             foreach (Move move in legalMoves)
             {
                 board.MakeMove(move);
+
                 if (move.IsCapture || board.IsInCheck())
                 {
-                    float eval = -quiescenceSearch(board, -beta, -alpha);
+                    float eval = -QuiescenceSearch(board, -beta, -alpha);
                     board.UndoMove(move);
-                    if (eval >= beta) return eval;
-                    if (eval < alpha) alpha = eval;
+
+                    if (eval >= beta)
+                    {
+                        return beta;
+                    }
+
+                    if (eval > alpha)
+                    {
+                        alpha = eval;
+                    }
                 }
-                else board.UndoMove(move);
+                else
+                {
+                    board.UndoMove(move);
+                }
             }
+
             return alpha;
         }
 
         public Move Think(Board board, Timer timer)
         {
-            int depth = 2;
-            List<MoveWithScore> movesWithScores = new List<MoveWithScore>();
             Move[] legalMoves = board.GetLegalMoves();
+
             if (legalMoves.Length == 1)
             {
                 return legalMoves[0];
             }
-            // Increases search depth if the following parameters are met.
-            if (SquareCounter(board.AllPiecesBitboard) < 14) depth += 1;
-            if (SquareCounter(board.AllPiecesBitboard) < 8) depth += 2;
-            if (SquareCounter(board.AllPiecesBitboard) < 5) depth += 2;
-            if (board.IsInCheck()) depth += 1;
-            if (timer.MillisecondsRemaining < 5000) depth -= 2;
-            if (timer.MillisecondsRemaining < 500) depth = 0;
+
             foreach (Move move in legalMoves)
             {
-                if (timer.MillisecondsRemaining < 1000)
-                {
-                    movesWithScores = movesWithScores.OrderByDescending(ms => ms.Score).ToList();
-                    return movesWithScores[0].Move;
-                }
                 board.MakeMove(move);
+
                 if (board.IsInCheckmate())
                 {
                     return move;
                 }
-                float score = -Negamax(board, depth, float.NegativeInfinity, float.PositiveInfinity);
-                board.UndoMove(move);
-                movesWithScores.Add(new MoveWithScore { Move = move, Score = score });
-            }
-            movesWithScores = movesWithScores.OrderByDescending(ms => ms.Score).ToList();
-            return movesWithScores[0].Move;
-        }
 
-        public struct MoveWithScore
-        {
-            public Move Move { get; set; }
-            public float Score { get; set; }
+                board.UndoMove(move);
+            }
+
+            int depth = 3;
+
+            if (SquareCounter(board.AllPiecesBitboard) < 14)
+            {
+                depth += 1;
+            }
+
+            if (SquareCounter(board.AllPiecesBitboard) < 8)
+            {
+                depth += 3;
+            }
+
+            if (SquareCounter(board.AllPiecesBitboard) < 5)
+            {
+                depth += 5;
+            }
+
+            if (board.IsInCheck())
+            {
+                depth += 1;
+            }
+
+            if (timer.MillisecondsRemaining < 5000)
+            {
+                depth -= 2;
+            }
+
+            if (timer.MillisecondsRemaining < 1000)
+            {
+                depth = 1;
+            }
+
+            (float bestScore, Move bestMove) = Negamax(board, depth, float.NegativeInfinity, float.PositiveInfinity);
+
+            return bestMove;
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -6,16 +6,45 @@ using System.Linq;
 using System.IO;
 public class MyBot : IChessBot
 {
-    private float Evaluate(Board board)
+    // Piece value constants for move ordering
+    private static readonly int[] PieceValues = {
+        0,      // None
+        100,    // Pawn
+        310,    // Knight
+        320,    // Bishop
+        500,    // Rook
+        900,    // Queen
+        int.MaxValue   // King
+    };
+
+    private float Evaluate(Board board) //Evaluates a single position without depth
     {
-        if (board.IsInCheckmate()) return float.NegativeInfinity;
-        if (board.IsDraw()) return 0;
+        if (board.IsInCheckmate())
+        {
+            return float.NegativeInfinity;
+        }
+
+        if (board.IsDraw())
+        {
+            return 0;
+        }
+
         float whiteScore = 0;
         float blackScore = 0;
         PieceList[] pieceLists = board.GetAllPieceLists();
-        int piececount = SquareCounter(board.AllPiecesBitboard);
+        int pieceCount = SquareCounter(board.AllPiecesBitboard);
+
         // Endgame is true if there are less than x pieces left
-        int endgame = (piececount < 16) ? 1 : -1;
+        int endgame;
+        if (pieceCount < 16)
+        {
+            endgame = 1;
+        }
+        else
+        {
+            endgame = -1;
+        }
+
         foreach (PieceList list in pieceLists)
         {
             foreach (Piece piece in list)
@@ -30,134 +59,290 @@ public class MyBot : IChessBot
                 }
             }
         }
-        return board.IsWhiteToMove ? (whiteScore - blackScore) : (blackScore - whiteScore);
+
+        if (board.IsWhiteToMove)
+        {
+            return whiteScore - blackScore;
+        }
+        else
+        {
+            return blackScore - whiteScore;
+        }
     }
+
     private float PieceEvaluator(Board board, Piece piece, int endgame)
     {
+        float pieceValue = GetPieceValue(piece);
         if (piece.IsPawn)
         {
-            float pawnValue = 100 + 2 * (endgame == 1 ? (board.IsWhiteToMove ? piece.Square.Rank : 7 - piece.Square.Rank) : 0);
-            return pawnValue;
+            if (endgame == 1)
+            {
+                int rank;
+                if (board.IsWhiteToMove)
+                {
+                    rank = piece.Square.Rank;
+                }
+                else
+                {
+                    rank = 7 - piece.Square.Rank;
+                }
+                pieceValue += 2 * rank;
+            }
+
+            return pieceValue;
         }
+
         if (piece.IsKnight)
         {
-            return 310 + SquareCounter(BitboardHelper.GetKnightAttacks(piece.Square));
+            return pieceValue + SquareCounter(BitboardHelper.GetKnightAttacks(piece.Square));
         }
+
         if (piece.IsBishop)
         {
-            return 320 + SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Bishop, piece.Square, board));
+            return pieceValue + SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Bishop, piece.Square, board));
         }
+
         if (piece.IsRook)
         {
-            return 500 + 0.5f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Rook, piece.Square, board));
+            return pieceValue + 0.5f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Rook, piece.Square, board));
         }
+
         if (piece.IsQueen)
         {
-            return 900 + 0.1f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Queen, piece.Square, board));
+            return pieceValue + 0.1f * SquareCounter(BitboardHelper.GetSliderAttacks(PieceType.Queen, piece.Square, board));
         }
-        // If nothing prior then king
+
+        // If nothing prior, then the piece must be a king
         return endgame * SquareCounter(BitboardHelper.GetKingAttacks(piece.Square));
     }
+
     private int SquareCounter(ulong bitboard)
     {
-        // Brian Kernighan's algorithm
         int count = 0;
+
         while (bitboard != 0)
         {
             bitboard &= (bitboard - 1);
             count++;
         }
+
         return count;
     }
+
+    // Estimate move score for ordering
+    private int EstimateMoveScore(Board board, Move move)
+    {
+        int score = 0;
+
+        // Captures - MVV/LVA (Most Valuable Victim / Least Valuable Aggressor)
+        if (move.IsCapture)
+        {
+            Piece capturedPiece = board.GetPiece(move.TargetSquare);
+            Piece movingPiece = board.GetPiece(move.StartSquare);
+
+            // Value of captured piece minus value of moving piece (divided by 100)
+            int captureValue = GetPieceValue(capturedPiece) - GetPieceValue(movingPiece) / 100;
+            score += 10000 + captureValue;
+        }
+
+        // Promotions
+        if (move.IsPromotion)
+        {
+            score += 9000 + (int)move.PromotionPieceType * 100;
+        }
+
+        // Castling
+        if (move.IsCastles)
+        {
+            score += 1000;
+        }
+
+        // Check if move gives check (approximation)
+        board.MakeMove(move);
+        if (board.IsInCheck())
+        {
+            score += 500;
+        }
+        board.UndoMove(move);
+
+        return score;
+    }
+
+    private int GetPieceValue(Piece piece)
+    {
+        if (piece.IsPawn) return PieceValues[1];
+        if (piece.IsKnight) return PieceValues[2];
+        if (piece.IsBishop) return PieceValues[3];
+        if (piece.IsRook) return PieceValues[4];
+        if (piece.IsQueen) return PieceValues[5];
+        if (piece.IsKing) return PieceValues[6];
+        return 0;
+    }
+
     private (float, Move) Negamax(Board board, int depth, float alpha, float beta)
     {
         if (board.IsInCheckmate())
         {
-            return (depth == 0) ? (float.NegativeInfinity, Move.NullMove) : (float.NegativeInfinity + (depth * 100), Move.NullMove);
-            // The closer the mate, the higher the score; subtracting by depth ensures that mate-in-1 is prioritized over mate-in-5
+            return (float.NegativeInfinity + (depth * 100), Move.NullMove);
         }
+
         if (board.IsDraw())
         {
-            return (0, Move.NullMove);  // Return null in a drawn position
+            return (0, Move.NullMove);
         }
+
         if (depth == 0)
         {
-            float finalEval = quiescenceSearch(board, alpha, beta);
-            return (finalEval, Move.NullMove);  // Return evaluation with no specific move
+            float finalEval = QuiescenceSearch(board, alpha, beta);
+            return (finalEval, Move.NullMove);
         }
+
         Move[] legalMoves = board.GetLegalMoves();
-        Move bestMove = Move.NullMove;  // Track the best move
+        Move bestMove = Move.NullMove;
         float bestEval = float.NegativeInfinity;
+
+        // Order moves
+        List<(Move move, int score)> scoredMoves = new List<(Move, int)>();
         foreach (Move move in legalMoves)
         {
+            int moveScore = EstimateMoveScore(board, move);
+            scoredMoves.Add((move, moveScore));
+        }
+        scoredMoves.Sort((a, b) => b.score.CompareTo(a.score)); // Sort in descending order
+
+        // Process ordered moves
+        foreach (var (move, _) in scoredMoves)
+        {
             board.MakeMove(move);
-            (float eval, _) = Negamax(board, depth - 1, -beta, -alpha);  // Recursive call to negamax
+            (float eval, _) = Negamax(board, depth - 1, -beta, -alpha);
             eval = -eval;
             board.UndoMove(move);
+
             if (eval > bestEval)
             {
                 bestEval = eval;
                 bestMove = move;
             }
+
             alpha = Math.Max(alpha, eval);
+
             if (alpha >= beta)
             {
-                break;  // Beta cutoff
+                break; // Beta cutoff
             }
         }
+
         return (bestEval, bestMove);
     }
-    private float quiescenceSearch(Board board, float alpha, float beta)
+
+    private float QuiescenceSearch(Board board, float alpha, float beta)
     {
-        // Evaluation without captures
-        float stand_pat = Evaluate(board);
-        if (stand_pat >= beta)
+        float standPat = Evaluate(board);
+
+        if (standPat >= beta)
         {
             return beta;
         }
-        if (stand_pat > alpha) alpha = stand_pat;
+
+        if (standPat > alpha)
+        {
+            alpha = standPat;
+        }
+
+        // Get captures and order them
         Move[] legalMoves = board.GetLegalMoves();
+        List<(Move move, int score)> scoredMoves = new List<(Move, int)>();
+
         foreach (Move move in legalMoves)
         {
-            board.MakeMove(move);
             if (move.IsCapture || board.IsInCheck())
             {
-                float eval = -quiescenceSearch(board, -beta, -alpha);
-                board.UndoMove(move);
-                if (eval >= beta) return eval;
-                if (eval < alpha) alpha = eval;
+                int moveScore = EstimateMoveScore(board, move);
+                scoredMoves.Add((move, moveScore));
             }
-            else board.UndoMove(move);
         }
+        scoredMoves.Sort((a, b) => b.score.CompareTo(a.score)); // Sort in descending order
+
+        foreach (var (move, _) in scoredMoves)
+        {
+            board.MakeMove(move);
+            float eval = -QuiescenceSearch(board, -beta, -alpha);
+            board.UndoMove(move);
+
+            if (eval >= beta)
+            {
+                return beta;
+            }
+
+            if (eval > alpha)
+            {
+                alpha = eval;
+            }
+        }
+
         return alpha;
     }
+
     public Move Think(Board board, Timer timer)
     {
         Move[] legalMoves = board.GetLegalMoves();
+
         if (legalMoves.Length == 1)
         {
             return legalMoves[0];
         }
+
         foreach (Move move in legalMoves)
         {
             board.MakeMove(move);
+
             if (board.IsInCheckmate())
+            {
                 return move;
+            }
+
             board.UndoMove(move);
         }
-        int depth = 3;
-        // Dynamically increase depth based on remaining pieces or timer
-        if (SquareCounter(board.AllPiecesBitboard) < 14) depth += 1;
-        if (SquareCounter(board.AllPiecesBitboard) < 8) depth += 2;
-        if (SquareCounter(board.AllPiecesBitboard) < 5) depth += 2;
-        if (board.IsInCheck()) depth += 1;
 
-        if (timer.MillisecondsRemaining < 5000) depth -= 2;
-        if (timer.MillisecondsRemaining < 1000) depth = 0;
+        int depth = 4;
 
-        // Run Negamax from the starting position, allowing it to return the best move and score
+        if (SquareCounter(board.AllPiecesBitboard) < 14)
+        {
+            depth += 1;
+        }
+
+        if (SquareCounter(board.AllPiecesBitboard) < 8)
+        {
+            depth += 3;
+        }
+
+        if (SquareCounter(board.AllPiecesBitboard) < 5)
+        {
+            depth += 5;
+        }
+
+        if (board.IsInCheck())
+        {
+            depth += 1;
+        }
+
+        if (timer.MillisecondsRemaining > 60000)
+        {
+            depth += 1;
+        }
+
+        if (timer.MillisecondsRemaining < 5000)
+        {
+            depth -= 2;
+        }
+
+        if (timer.MillisecondsRemaining < 1000)
+        {
+            depth = 1;
+        }
+
         (float bestScore, Move bestMove) = Negamax(board, depth, float.NegativeInfinity, float.PositiveInfinity);
 
-        return bestMove;  // Return the best move found
+        return bestMove;
     }
 }
